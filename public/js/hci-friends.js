2866eff155d1c2e4df2c1bc599d36f623c3c729b
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2016-02-04 10:31:55 -0800

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,14 +10,19 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
+  $('.friend h3 a').click(function(e) {
+    e.preventDefault();
+    $(this).text(anagrammedName($(this).text()));
+  });
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,17 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
+exports.addFriend = function(req, res) { 
+	var name = req.query.name,
+      description = req.query.name;
+
+  data.friends.push({
+    name: name,
+    description: description,
+    imageURL: 'http://lorempixel.com/400/400'
+  })
+
+  res.render('add', {
+    name: name,
+    description: description
+  })
  }
